7484aa23a68ce23646cfba6f3b676683fa9d65c5
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2022-12-28 12:20:00 -0500

    added scraping and openai

---
+++ index.js
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer";
+import { Configuration, OpenAIApi } from "openai";
+
+// Scraping
+const URL = 'https://www.secondnature.com/company'
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+await page.goto(URL);
+
+const paragraph = await page.$("#guys-behind p.paragraph");
+const paragraphContent = await page.evaluate(body => body.innerHTML, paragraph);
+await paragraph.dispose();
+
+await browser.close();
+
+console.log(paragraphContent);
+
+
+
+// Open Ai
+const configuration = new Configuration({
+    apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const prompt = `Decide whether the following text is positive, neutral or negative. Text: ${paragraphContent}. Sentiment:`
+ 
+const completion = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: prompt,
+  temperature: 0.6,
+});
+
+console.log(`\nSentiment:`, completion.data.choices[0].text);
