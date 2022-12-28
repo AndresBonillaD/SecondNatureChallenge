8bed93546502b2cf1114f6ebc7b5623c4334ccc4
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2022-12-28 12:34:13 -0500

    updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SecondNatureChallenge
 Second Nature Coding Challenge Miercoles 28 2022
+
+## Execution Instructions
+```
+npm install
+node index.js
+```
--- index.js
+++ index.js
@@ -5,34 +5,40 @@ import {} from 'dotenv/config';
 // Scraping
 const URL = 'https://www.secondnature.com/company'
 
+// Initialize browser
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
+// Go To URL
 await page.goto(URL);
 
+// Get HTML element and content using query selector
 const paragraph = await page.$("#guys-behind p.paragraph");
 const paragraphContent = await page.evaluate(body => body.innerHTML, paragraph);
-await paragraph.dispose();
 
+// Closing instances
+await paragraph.dispose();
 await browser.close();
 
+// Print results
 console.log(paragraphContent);
 
-
-
 // Open Ai
+// Open Ai Configuration
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+// OpenAi Instance
 const openai = new OpenAIApi(configuration);
-
 const prompt = `Decide whether the following text is positive, neutral or negative. Text: ${paragraphContent}. Sentiment:`
  
+// Model Application
 const completion = await openai.createCompletion({
   model: "text-davinci-003",
   prompt: prompt,
   temperature: 0.6,
 });
 
+// Print Sentiment
 console.log(`\nSentiment:`, completion.data.choices[0].text);
